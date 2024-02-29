1c096fe40e70a845d3ddf4eaabbfa4f9c7b25e1b
Subrat Gupta <85225011+iSubrat@users.noreply.github.com>
2024-02-29 19:42:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,8 +17,32 @@ def send_email(sender_email, sender_password, recipient_email, subject, message)
         email_message['To'] = recipient_email
         email_message['Subject'] = subject
 
-        # Attach the message body
-        email_message.attach(MIMEText(message, 'plain'))
+        # Styling
+        html_message = f"""
+        <html>
+            <body style="background-color: #E72A73; color: white; font-family: Arial, sans-serif;">
+                <div style="text-align: center;">
+                    <img src="cid:logo" alt="Web2App Logo" style="max-width: 150px;"><br><br>
+                    <h2 style="color: white;">Web2App: Web to App Converter</h2>
+                    <p>Dear {recipient_email},</p>
+                    <p>Congratulations! Your app is ready to download. Please follow the link below:</p>
+                    <p><a href="http://appcollection.in/InstantWeb2App/downloads/0101_Youtube_app.apk" style="color: white;">Download Your App</a></p>
+                    <p>Username: {{ username }}</p>
+                    <p>App Name: {{ appname }}</p>
+                    <p>Sender: Subrat Gupta - Web2App Team</p>
+                    <p style="font-size: 14px;">Publish your app on the Play Store for just $50!</p>
+                </div>
+            </body>
+        </html>
+        """
+        email_message.attach(MIMEText(html_message, 'html'))
+
+        # Attach the logo
+        with open('logo.png', 'rb') as logo:
+            logo_content = logo.read()
+            logo_part = MIMEImage(logo_content)
+            logo_part.add_header('Content-ID', '<logo>')
+            email_message.attach(logo_part)
 
         # Create SMTP session for sending the mail
         with smtplib.SMTP_SSL(smtp_host, smtp_port) as session:
